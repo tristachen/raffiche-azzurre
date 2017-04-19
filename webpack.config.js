7fff27bb41b3cbb968cf0f92f975b3a4e27aad06
trista.chen <trista.chen@ubitus.net>
2017-04-19 17:13:53 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path              = require('path');
+const webpack           = require('webpack');
+const webpackMerge      = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackDevServer  = require('webpack-dev-server');
+const nib               = require('nib');
+
+const config = {
+  base: {
+    entry: path.resolve(__dirname, 'scripts/index.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'content.js'
+    },
+    module: {
+      rules: [{
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 100000 }
+        },
+      }, {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }, {
+        test: /\.json$/,
+        use: 'json-loader'
+      }]
+    },
+    plugins: [
+      new ExtractTextPlugin('content.css')
+    ]
+  },
+
+
+
+
+
+  /**
+   *
+   * dev config
+   *
+   */
+  dev: {
+    module: {
+      rules: [{
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              sourceMap: true
+            }
+          }, {
+            loader: 'stylus-loader',
+            options: {
+              use: [nib()],
+              import: ['~nib/lib/nib/index.styl'],
+              sourceMap: true
+            }
+          }]
+        })
+      }]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+    devtool: 'sourcemap'
+  },
+
+
+
+
+
+  /**
+   *
+   * prod config
+   *
+   */
+  prod: {
+    module: {
+      rules: [{
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              minimize: true
+            }
+          }, {
+            loader: 'stylus-loader',
+            options: {
+              use: [nib()],
+              import: ['~nib/lib/nib/index.styl']
+            }
+          }]
+        })
+      }]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: true,
+        }
+      })
+    ]
+  }
+};
+
+module.exports = function buildConfig(env) {
+  return webpackMerge(config.base, config[env]);
+};
