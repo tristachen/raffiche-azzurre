84a20162b407342acdf359a059aa232b3c3c030e
Trista Chen <trista.chen.515@gmail.com>
2017-05-04 17:51:54 +0800

    [facilities trainer] adjust code

--- scripts/views/facilities-trainer.js
+++ scripts/views/facilities-trainer.js
@@ -1,77 +1,81 @@
-import * as request from '../utils/request.js';
+/*
+ * - 加上額外資訊 [appendExtraInfo]
+ *   - 名稱欄
+ *     - ★ 能力值 | 天賦 | 年齡
+ *     - ◆ 特殊屬性
+ *   - 每個能力欄
+ *     - + 0.xxx 星, 最大值要特別 highlight
+ */
+
 import React from '../utils/react-like.js';
+import * as utils from '../utils/common.js';
+import * as request from '../utils/request.js';
 import htmlParser from '../helpers/htmlParser.js';
 
-const getTrain = (exp, i) => {
-  let c1 = 0,
-      c2 = 0,
-      c3 = 0,
-      c4 = 0,
-      c5 = 0; // 依次：阻擋，搶球，傳球，射門，戰術
-  if (location.href.indexOf('train-goalkeeping') >= 0) {
-    c1 = 0.724;
-    c2 = 0.0;
-    c3 = 0.115;
-    c4 = 0.0;
-    c5 = 0.163;
-  } else if (location.href.indexOf('train-defense') >= 0) {
-    c1 = 0.207;
-    c2 = 0.765;
-    c3 = 0.015;
-    c4 = 0.175;
-    c5 = 0.439;
-  } else if (location.href.indexOf('train-midfield') >= 0) {
-    c1 = 0.055
-    c2 = 0.218;
-    c3 = 0.695;
-    c4 = 0;
-    c5 = 0.307;
-  } else if (location.href.indexOf('train-attack') >= 0) {
-    c1 = 0.002;
-    c2 = 0.205;
-    c3 = 0.16;
-    c4 = 0.736;
-    c5 = 0.189;
+const getTrain = (cost, i) => {
+  //blocking, dueling, passing, scoring, tactics
+  const C_GOALKEEPING = [0.724, 0.000, 0.115, 0.000, 0.163],
+        C_DEFENSE     = [0.207, 0.765, 0.015, 0.175, 0.439],
+        C_MIDFIELD    = [0.055, 0.218, 0.695, 0.000, 0.307],
+        C_ATTACK      = [0.002, 0.205, 0.160, 0.736, 0.189];
+  let coefficient;
+  if (location.href.search('train-goalkeeping') >= 0) {
+    coefficient = C_GOALKEEPING;
+  } else if (location.href.search('train-defense') >= 0) {
+    coefficient = C_DEFENSE;
+  } else if (location.href.search('train-midfield') >= 0) {
+    coefficient = C_MIDFIELD;
+  } else if (location.href.search('train-attack') >= 0) {
+    coefficient = C_ATTACK;
   }
-  const c = [c1, c2, c3, c4, c5];
-  return 1000 * c[i] / exp;;
-}
+  return (1000 * coefficient[i] / cost).toFixed(3);
+};
 
+const appendExtraInfo = () => {
+  const elBodyRow = document.querySelectorAll('.horizontal_table tbody tr');
 
-if (location.href.search('specials') < 0) {
-  const trs = document.querySelectorAll('.horizontal_table tbody tr');
-  trs.forEach(tr => {
-    const playerHref = tr.querySelector('td:first-child > a:nth-child(2)').href;
+  const appendPlayerInfo = (elRow, info) => {
+    elRow.querySelector('td:first-child div').hidden = true;
+    elRow.querySelector('td:first-child').appendChild(
+      <div>{'★ {0} | {1} | {2}'.format(info.value, info.talent, info.age)}</div>
+    );
+    elRow.querySelector('td:first-child').appendChild(
+      <div>{'◆ [{0}]'.format(info.special_attributes)}</div>
+    );
+  };
 
-    request.get(playerHref)
-      .then(data => data.text())
-      .then(text => {
-        const parser = new DOMParser(),
-              doc = parser.parseFromString(text, 'text/html'),
-              el = doc.querySelector('.center'),
-              playerInfo = htmlParser(el);
+  const appendTrainInfo = elRow => {
+    const elCosts = elRow.querySelectorAll('.cost'),
+          costs = [];
+    elCosts.forEach((el, i) => {
+      const cost = parseInt(el.textContent.replace(/\D/ig, ''), 10),
+            train = getTrain(cost, i);
+      costs[costs.length] = train;
+      el.parentElement.parentElement.appendChild(
+        <div style='color: blue'>{'+ {0} 星'.format(train)}</div>
+      );
+    });
 
-        tr.querySelector('td:first-child div').hidden = true;
-        tr.querySelector('td:first-child').appendChild(
-          <div>{'★ ' + playerInfo.value + ' | ' + playerInfo.talent + ' | ' + playerInfo.age}</div>
-        );
-        tr.querySelector('td:first-child').appendChild(
-          <div>{'◆ [' + playerInfo.special_attributes + ']'}</div>
-        );
+    //highlight max train
+    const iMax = costs.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0),
+          elMaxCost = elCosts[iMax].parentElement.parentElement.lastChild;
+    elMaxCost.style.color = '#990000';
+    elMaxCost.style.fontWeight = 'bold';
+  };
 
-        const exps = [];
-        tr.querySelectorAll('.cost').forEach((el, i) => {
-          const exp = parseInt(el.textContent.replace(/\D/ig, ''), 10);
-          const value = getTrain(exp, i);
-          exps[exps.length] = value;
-          el.parentElement.parentElement.appendChild(<div style='color: blue'>{'+ ' + value.toFixed(3) + ' 星'}</div>);
-        });
-
-        const idxMax = exps.reduce((idxMax, x, i, arr) => x > arr[idxMax] ? i : idxMax, 0);
-        const elTrain = tr.querySelectorAll('.cost')[idxMax].parentElement.parentElement.lastChild;
-        elTrain.style.color = '#990000';
-        elTrain.style.fontWeight = 'bold';
-      })
-      .catch(err => console.log(err));
+  elBodyRow.forEach(tr => {
+    const playerUrl = tr.querySelector('td:first-child a:nth-child(2)').href;
+    request.get(playerUrl).then(doc => {
+      const el = doc.querySelector('div.center'),
+            playerInfo = htmlParser(el);
+      appendPlayerInfo(tr, playerInfo);
+      appendTrainInfo(tr);
+    })
+    .catch(err => console.log(err));
   });
+};
+
+//don't apply on facilities/trainer/specials
+if (location.href.search('specials') < 0) {
+  appendExtraInfo();
 }
