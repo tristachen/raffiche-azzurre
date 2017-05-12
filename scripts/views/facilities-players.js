6507534d32baaa2b2b5a17dfdf741f69ef8b9ad7
Trista Chen <trista.chen.515@gmail.com>
2017-05-12 11:42:36 +0800

    [facilities-players] append info

--- scripts/views/facilities-players.js
+++ scripts/views/facilities-players.js
@@ -1,19 +1,29 @@
 /*
- * - 加上球隊、截止時間資訊 [appendExtraInfo]
+ * - 加上截止時間、掛牌價 [appendExtraInfo]
  */
 
 import React from '../utils/react-like.js';
+import * as request from '../utils/request.js';
+import * as playerDom from '../helpers/player-dom.js';
 
 const appendExtraInfo = () => {
   const elHeadRow = document.querySelector('.horizontal_table thead tr');
-  elHeadRow.appendChild(<th>{chrome.i18n.getMessage('player_team')}</th>);
-  elHeadRow.appendChild(<th>截止時間</th>);
+  elHeadRow.appendChild(<th>{chrome.i18n.getMessage('player_transfer_deadline')}</th>);
+  elHeadRow.appendChild(<th>{chrome.i18n.getMessage('player_bid_price')}</th>);
 
-  const elBodyRow = document.querySelectorAll('.horizontal_table tbody tr');
-  elBodyRow.forEach(el => {
-    el.appendChild(<td></td>);
-    el.appendChild(<td></td>);
+  const elBodyTd = document.querySelectorAll('.horizontal_table tbody tr a[href *=transfer]');
+  elBodyTd.forEach(el => {
+    request.get(el.href).then(doc => {
+      const elDeadline = [].find.call(doc.querySelectorAll('th'), el => el.textContent === '期限'),
+            elBidPrice = doc.querySelector('.sub_block .money-positive'),
+            deadline = elDeadline.nextElementSibling.textContent,
+            bidPrice = elBidPrice.textContent;
+      el.parentElement.parentElement.appendChild(<td>{deadline}</td>);
+      el.parentElement.parentElement.appendChild(<td>{bidPrice}</td>);
+    });
   });
 };
 
 appendExtraInfo();
+playerDom.addTooltips();
+
